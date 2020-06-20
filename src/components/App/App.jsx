f893a38518b4bcb6e629760971ea34f8a87f0384
Andrii <drew24n@gmail.com>
2020-06-20 21:51:21 +0300

    init

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.scss";
+
+export class App extends React.Component {
+    render() {
+        return (
+            <main className="app">
+            </main>
+        )
+    }
+}
---
+++ src/components/common/Preloader/Preloader.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Preloader.scss";
+
+export const Preloader = () => {
+    return (
+        <div className={"spinner-container"}>
+            <div className={"spinner"}/>
+        </div>
+    )
+}
